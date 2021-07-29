40b9b016ba0520a74ee65690c05a8415b57950f0
cjsmith33 <charleswsmith8@gmail.com>
2021-07-29 09:16:21 -0700

    adding some additional exercises for try catch

--- src/main/java/com/chucksmith/Application.java
+++ src/main/java/com/chucksmith/Application.java
@@ -2,29 +2,57 @@ package com.chucksmith;
 
 public class Application {
 
+//    public static void doIt(int bar) {
+//        int foo = 0;
+//        int baz = 0;
+//
+//        try {
+//            foo = bar / baz;
+//        } catch (NullPointerException ex) {
+//            System.out.println("Something blew up!");
+//        } finally {
+//            System.out.println("Done");
+//        }
+//
+//        System.out.println("foo: " + foo);
+//    }
+
     public static void main(String[] args) {
-        int[] numbers = new int[]{1, 3, 0, 7, 9};
+        //doIt(5);
 
-        System.out.println(numbers[2]);
-        try {
-            // if you change to numbers[2] you get a 'bad math' error
-            // if you change to numbers[8] you get a 'out of bounds' error
-            int num = numbers[8];
-            int result = 4 / num;
-            //System.out.println(numbers[7]);
-        } catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("out of bounds");
-        } catch (ArithmeticException e){
-            System.out.println("bad math");
-        }
-        catch (Exception e){
-            int foo = 4 / 0;
-            System.out.println("something went wrong");
-        } finally {
-            System.out.println("finally");
-        }
+//        try {
+//            result = riskyProcess.run(input);
+//        } catch (NullPointerException e) {
+//            System.out.println("encountered null pointer");
+//        } catch (IndexOutOfBoundsException e) {
+//            System.out.println("index out of bounds");
+//        } finally {
+//            System.out.println("Done");
+//        }
+//    }
 
-        System.out.println("Done");
-    }
+//        int[] numbers = new int[]{1, 3, 0, 7, 9};
+//
+//        System.out.println(numbers[2]);
+//        try {
+//            // if you change to numbers[2] you get a 'bad math' error
+//            // if you change to numbers[8] you get a 'out of bounds' error
+//            int num = numbers[8];
+//            int result = 4 / num;
+//            //System.out.println(numbers[7]);
+//        } catch (ArrayIndexOutOfBoundsException e){
+//            System.out.println("out of bounds");
+//        } catch (ArithmeticException e){
+//            System.out.println("bad math");
+//        }
+//        catch (Exception e){
+//            int foo = 4 / 0;
+//            System.out.println("something went wrong");
+//        } finally {
+//            System.out.println("finally");
+//        }
+//
+//        System.out.println("Done");
+}
 
 }
