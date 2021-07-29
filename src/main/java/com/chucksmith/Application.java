2bf66150aff5eabc5b9c3b877fc380f699c7e0e3
cjsmith33 <charleswsmith8@gmail.com>
2021-07-29 09:15:42 -0700

    adding information for try catch exercises

--- src/main/java/com/chucksmith/Application.java
+++ src/main/java/com/chucksmith/Application.java
@@ -1,4 +1,30 @@
 package com.chucksmith;
 
 public class Application {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[]{1, 3, 0, 7, 9};
+
+        System.out.println(numbers[2]);
+        try {
+            // if you change to numbers[2] you get a 'bad math' error
+            // if you change to numbers[8] you get a 'out of bounds' error
+            int num = numbers[8];
+            int result = 4 / num;
+            //System.out.println(numbers[7]);
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("out of bounds");
+        } catch (ArithmeticException e){
+            System.out.println("bad math");
+        }
+        catch (Exception e){
+            int foo = 4 / 0;
+            System.out.println("something went wrong");
+        } finally {
+            System.out.println("finally");
+        }
+
+        System.out.println("Done");
+    }
+
 }
